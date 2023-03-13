21f5d6bb8126955a5eca905033ec1615ce719bd7
Firstname Lastname <radjawarior@mail.ru>
2023-03-13 06:07:47 +0200

    Test commit 13\03\23

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/footer/Footer";
+import {defaultHero} from "./utils/constants";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [hero, setHero] = useState(defaultHero);
+    const changeHero = hero => {
+        setHero(hero);
+    }
+
+    return (
+        <div className='container-fluid'>
+            <Header hero={hero}/>
+            <Main changeHero={changeHero}/>
+            <Footer/>
+        </div>
+    );
+
 }
 
 export default App;
+
+//URI = URL + UDN
+//npm i react-router-dom@5
