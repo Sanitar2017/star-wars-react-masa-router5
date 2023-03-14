460c2bf62058266896845251af5549fde19509e2
Ivan Kovalenko <alivankovalenko@gmail.com>
2023-03-14 11:31:07 +0200

    add comment

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 import {base_url, periodMonth} from "../../utils/constants";
 
 const Contact = () => {
-
+    //comment
     const [planetsArray, setPlanets] = useState(['wait...']);
     const fillPlanets = (url) => {
         fetch(url)
